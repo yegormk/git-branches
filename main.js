655698b3bc4e048b234a807f97e046a6d170d88a
Yehor <yegor02092002@gmail.com>
2022-10-10 14:41:59 +0300

    :wq +1 cool variable

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const dimasVariable = 'FIRST VARIABLE IN MAIN!';
-
+const yegoretsVariable = 'Coolest variable yet :D'
 console.log(dimasVariable);
+console.log(yegoretsVariable)
+console.log('Some changes!')

64b1cd0efd33683ff229348d8b66c6f890c913ee
Harkunov Dmytro <harkunovdmytro@gmail.com>
2022-10-10 20:40:21 +0300

    cleared file main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const dimasVariable = "FIRST VARIABLE IN MAIN!";
-
-const yegoretsVariable = "Coolest variable yet :D";
-console.log(dimasVariable);
-console.log(yegoretsVariable);
-console.log("Some changes!!!");
-const isSecondFeature = false;
-const isFeature = true;
